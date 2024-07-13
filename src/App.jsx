9c2a9e4844d5771a9cea36a1a667f6396f5875db
Jarrad L <jlovelace2500k@gmail.com>
2024-07-13 12:59:46 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Calculator from './components/Calculator'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Calculator App</h1>
+      <Calculator />
     </>
   )
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,14 @@
+import { useState, useRef, useEffect } from 'react'
+
+import '../App.css'
+function Calculator() {
+
+  return (
+    <div>
+      <h1>Calculator App</h1>
+    </div>
+  )
+
+}
+
+export default Calculator
